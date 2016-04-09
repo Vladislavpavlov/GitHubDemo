f16dabe3270d373c8f2c094e5e3afb225a5386cd
Vladislav Pavlov <vladi_terziiski@abv.bg>
2016-04-09 13:08:59 +0300

    p

--- ssdg/ssdg/Program.cs
+++ ssdg/ssdg/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ssdg
+namespace ConsoleApplication2
 {
-    class Program
+    class PrintNumbers
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("1");
+            Console.WriteLine("11");
+            Console.WriteLine("1001");
         }
     }
 }
