73e256409c1407e5575384317e4b4664a31f1785
pepi983 <pepi983@abv.bg>
2016-04-09 15:51:51 +0300

    popravka v programata

--- ssdg/ssdg/Program.cs
+++ ssdg/ssdg/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApplication2
         static void Main()
         {
             Console.WriteLine("1");
-            Console.WriteLine("11");
+            Console.WriteLine("101");
             Console.WriteLine("1001");
         }
     }
